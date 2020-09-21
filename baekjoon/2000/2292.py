2b1e2692cc76e40fec4465323a54571490b2f89b
YangDongJae_Ministry of Health and Welfare <ydj9805@me.com>
2020-09-21 10:38:00 +0900

    solve the problem use list(range()) function"

---
+++ baekjoon/2000/2292.py
@@ -0,0 +1,41 @@
+"""
+Created on Mon Sep 21 09:21:06 2020
+
+@author: Mohw-IN
+"""
+# key of result = Sum_n=1 ^ limit N_t - 1 + N_t + 6_n
+import re
+
+data = eval(input())
+
+data_range = 2
+
+update_data_range = 1 + 6 
+
+result = 0
+
+if data < 1 or data >= 1000000000:
+    print('error! please check your input data range!')
+
+else:
+    range_list = list(range(data_range , update_data_range))
+
+    for i in range ( 1, 10):
+        if data == 1:
+            print('0')
+        
+        elif data in range_list:
+            print(i)
+            break
+
+        else:
+            result += 1
+            data_range = update_data_range + 1
+            update_data_range = (data_range + 6 * (result + 1)) - 1
+            range_list = list(range(data_range , update_data_range + 1))
+            print(data_range , " : " , update_data_range)
+            print(range_list)
+            print(i)
+            print("---")
+
+        
