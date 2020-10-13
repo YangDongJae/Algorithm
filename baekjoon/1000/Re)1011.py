adb3c5ec627a94e90ac5d79e1884357dd18ec18b
YangDongJae_Ministry of Health and Welfare <ydj9805@me.com>
2020-10-13 11:08:29 +0900

    feat : backjoon 1011 : i have to re try own myself to imporve my algorithm skills

---
+++ baekjoon/1000/Re)1011.py
@@ -0,0 +1,18 @@
+for i in range (eval(input())):
+    a , b = map(int, input.split())
+
+    distance = b - a
+    handler = 1
+
+    while True:
+
+        if handler ** 2 < handler <= (handler + 1 ** 2):
+            break
+        handler += 1
+
+    if handler ** 2 == distance:
+        print(handler - 1)
+    elif handler ** 2 < handler <= handler ** 2 + handler:
+        print(handler * 2)
+    else:
+        print((handler * 2) + 1)
