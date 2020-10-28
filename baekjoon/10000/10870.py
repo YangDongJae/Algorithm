edb3b3aac379a14e0cc9cbf7c44bed8034b32f0a
YangDongJae_Ministry of Health and Welfare <ydj9805@me.com>
2020-10-28 09:47:33 +0900

    feat : Add backjoon quiz number of 10870 with recursive fuctino

---
+++ baekjoon/10000/10870.py
@@ -0,0 +1,12 @@
+def fibo(n):
+    if n>= 2:
+        return fibo(n - 1) + fibo( n - 2)
+    else:
+        return n
+
+    # return fibo(n-1) + fibo(n-2) if n >= 2 else n
+
+for n in range(1, 11):
+    print(n, fibo(n))
+
+    #ask for prof
