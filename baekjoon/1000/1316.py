04e4a76623749b8bb356ac0c9655d247ce6bcf8f
YangDongJae <ydj9805@me.com>
2022-02-24 21:50:43 +0900

    Solve Algorithm Quiz : refer - https://ooyoung.tistory.com/79

--- baekjoon/1000/1316.py
+++ baekjoon/1000/1316.py
@@ -1,21 +1,19 @@
 def test(word):
-  point = word[0]
-  for i in range (1,len(word)):
-    if word[i] != point:
-      if point in word[i+1:]:
+  for i in range (len(word)-1):
+    if word[i] != word[i+1]:
+      check_list = list(word[i + 2:])
+      if check_list.count(word[i]):
         return False
         break
-  if len(word) > 1:
-    test(word[1:])
-
 
 count = int(input())
+
 return_value = 0
 
 for i in range (count):
-  word = input()
-
-  if test(word) is not False:
-    return_value +=1
+  
+  data = input()
+  if test(data) is not False:
+    return_value += 1
 
 print(return_value)
