f7c39b95b53fe12d454e8daebc6692a2b5d645cf
YangDongJae <ydj9805@me.com>
2022-03-25 08:55:40 +0900

    test

--- baekjoon/1000/1002.py
+++ baekjoon/1000/1002.py
@@ -2,17 +2,21 @@ for _ in range (int(input())):
   x1, y1, r1, x2, y2, r2 = map(int,input().split())
 
   dist = ((x2 - x1)**2 + (y2 - y1)**2)** 0.5
+  subtraction = (r1 - r2)**2
+  summation = (r1 + r2)**2
 
-  if x1 == y1 and x2 == y2 and r1 == r2:
+  if x1 == x2 and y1 == y2 and r1 == r2:
     print(-1)
 
-  elif (r2 - r1) < dist < (r1 + r2):
-    print(2)
-
-  elif r2 - r1 == dist or r1 + r2 == dist:
-    print(1)
-
-  elif (r1 + r2) < dist or dist < (r2 - r1) or dist == 0:
+  elif x1 == y1 and x2 == y2 and r1 != r2:
     print(0)
 
-# abnormal case 1 0 0 9 1 0 1
+  elif summation < dist or dist < subtraction:
+    print(0)    
+
+  elif subtraction < dist and subtraction < summation:
+    print(2)
+
+  elif summation == dist or subtraction == dist:
+    print(1)
+
